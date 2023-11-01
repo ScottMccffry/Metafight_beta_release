ee365768f809c23bf8cc0445722487aaef28b49c
Armand <armand.fatalot@gmail.com>
2023-11-01 09:41:29 +0100

    flask server port5000

--- backend/main.py
+++ backend/main.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     # Start the event listener in a separate process
     #Process(target=eventListener.main).start()
     #before it was app.run(debug=True)
-    socketio.run(app, host='0.0.0.0', port=80, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
